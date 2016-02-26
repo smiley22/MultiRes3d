d78411b885a061cd98401cd91c4fdf126ba5d9e8
Torben Könke <torben.koenke@gmail.com>
2016-02-26 14:17:35 +0100

    SpriteTextRenderer Bibliothek hinzufügen

--- MultiRes3d/Program.cs
+++ MultiRes3d/Program.cs
@@ -1,6 +1,7 @@
 ﻿using SlimDX.Windows;
 using System;
 using System.Windows.Forms;
+using SlimDX.Direct3D11;
 
 namespace MultiRes3d {
 	static class Program {
@@ -11,11 +12,20 @@ namespace MultiRes3d {
 		static void Main() {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			var form = new Window();
-			// MessagePump verarbeitet alle eingehenden Fensternachrichten und ruft
-			// kontinuierlich form.Run auf, wenn es keine Nachrichten zu verarbeiten
-			// gibt.
-			MessagePump.Run(form, form.Run);
+		    try {
+		        var form = new Window();
+                // MessagePump verarbeitet alle eingehenden Fensternachrichten und ruft
+                // kontinuierlich form.Run auf, wenn es keine Nachrichten zu verarbeiten
+                // gibt.
+                MessagePump.Run(form, form.Run);
+            }
+            catch (Direct3D11Exception) {
+		        MessageBox.Show(
+                    "Please make sure you have installed the DirectX End-User Runtimes (June 2010) " +
+                    "available from Microsoft at" + Environment.NewLine + Environment.NewLine +
+                    "https://www.microsoft.com/en-us/download/details.aspx?id=8109",
+		            "Direct3D 11 Initialization failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+		    }
 		}
 	}
 }
